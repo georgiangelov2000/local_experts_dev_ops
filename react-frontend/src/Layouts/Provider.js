7ea81e93f4c47c19ce7417c0b10dcacc98d7e1a4
georgiangelov2000 <gangelov2000@gmail.com>
2025-07-01 19:52:24 +0300

    _

--- react-frontend/src/Layouts/Provider.js
+++ react-frontend/src/Layouts/Provider.js
@@ -99,7 +99,7 @@ export default function Provider() {
           </div>
           <button
             onClick={() => setShowContact(true)}
-            className="mt-4 sm:mt-0 text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5"
+            className="mt-4 sm:mt-0 text-white bg-blue-700 hover:bg-blue-800 font-medium text-sm px-5 py-2.5 cursor-pointer"
           >
             Contact
           </button>
