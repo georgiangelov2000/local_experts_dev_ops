a1350cc18b031611d49f0cc7ca960536c9addec5
georgiangelov2000 <gangelov2000@gmail.com>
2025-07-03 19:02:59 +0300

    _

--- src/database/migrations/0001_01_01_000000_create_users_table.php
+++ src/database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
                 $table->string('name');
                 $table->string('email')->unique();
                 $table->string('password');
-                $table->string('alias')->unique();
                 $table->tinyInteger('is_active')->default(1);
                 $table->timestamp('email_verified_at')->nullable();
                 $table->rememberToken();
